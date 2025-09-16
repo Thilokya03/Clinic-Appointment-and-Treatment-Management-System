0247c210334aea07ce324c97158e9bef00ca22eb
Thilokya Angeesa <thilokyaangeesa@gmail.com>
2025-09-16 12:27:31 +0530

    Add Home page

--- CATMS/src/App.jsx
+++ CATMS/src/App.jsx
@@ -3,13 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Login from './pages/Login/Login'
+import Home from './pages/Home/Home'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Login />
+      <Home />
     </>
   )
 }
